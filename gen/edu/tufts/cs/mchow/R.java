c9ecc97ff034fb112fbbcd5cd50515b24a8fce3e
jaltreuter <jaltreuter@gmail.com>
2012-01-24 00:23:33 -0500

    build fixes

--- gen/edu/tufts/cs/mchow/R.java
+++ gen/edu/tufts/cs/mchow/R.java
@@ -8,7 +8,13 @@
 package edu.tufts.cs.mchow;
 
 public final class R {
+    public static final class array {
+        public static final int tap_sensitivity=0x7f050000;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int transparent=0x7f060000;
     }
     public static final class drawable {
         public static final int about=0x7f020000;
@@ -20,57 +26,114 @@ public final class R {
         public static final int ball_red=0x7f020006;
         public static final int ball_yellow=0x7f020007;
         public static final int blank=0x7f020008;
-        public static final int box_blue=0x7f020009;
-        public static final int box_brown=0x7f02000a;
-        public static final int box_green=0x7f02000b;
-        public static final int box_red=0x7f02000c;
-        public static final int box_yellow=0x7f02000d;
-        public static final int boxes=0x7f02000e;
-        public static final int difficulty=0x7f02000f;
-        public static final int explosion=0x7f020010;
-        public static final int gameover=0x7f020011;
-        public static final int highscores=0x7f020012;
-        public static final int hiscoretext=0x7f020013;
-        public static final int howto=0x7f020014;
-        public static final int howto2=0x7f020015;
-        public static final int icon=0x7f020016;
-        public static final int icon_hd=0x7f020017;
-        public static final int left=0x7f020018;
-        public static final int mars=0x7f020019;
-        public static final int menuscreen=0x7f02001a;
-        public static final int mothership=0x7f02001b;
-        public static final int numbers=0x7f02001c;
-        public static final int pdouble=0x7f02001d;
-        public static final int pluto=0x7f02001e;
-        public static final int pshield=0x7f02001f;
-        public static final int right=0x7f020020;
-        public static final int ship1=0x7f020021;
-        public static final int ship1b=0x7f020022;
-        public static final int shot=0x7f020023;
-        public static final int shot2=0x7f020024;
-        public static final int shot3=0x7f020025;
-        public static final int shot3b=0x7f020026;
-        public static final int shot4=0x7f020027;
-        public static final int shot4b=0x7f020028;
-        public static final int shot5=0x7f020029;
-        public static final int shot5b=0x7f02002a;
-        public static final int shot5c=0x7f02002b;
-        public static final int split=0x7f02002c;
-        public static final int startplay=0x7f02002d;
-        public static final int vert=0x7f02002e;
-        public static final int youwin=0x7f02002f;
+        public static final int boss0=0x7f020009;
+        public static final int box_blue=0x7f02000a;
+        public static final int box_brown=0x7f02000b;
+        public static final int box_green=0x7f02000c;
+        public static final int box_red=0x7f02000d;
+        public static final int box_yellow=0x7f02000e;
+        public static final int boxes=0x7f02000f;
+        public static final int difficulty=0x7f020010;
+        public static final int explosion=0x7f020011;
+        public static final int filled_box=0x7f020012;
+        public static final int gameover=0x7f020013;
+        public static final int highscores=0x7f020014;
+        public static final int hiscoretext=0x7f020015;
+        public static final int howto=0x7f020016;
+        public static final int howto2=0x7f020017;
+        public static final int icon=0x7f020018;
+        public static final int icon_hd=0x7f020019;
+        public static final int left=0x7f02001a;
+        public static final int mars=0x7f02001b;
+        public static final int menuscreen=0x7f02001c;
+        public static final int mothership=0x7f02001d;
+        public static final int numbers=0x7f02001e;
+        public static final int pdouble=0x7f02001f;
+        public static final int pluto=0x7f020020;
+        public static final int pshield=0x7f020021;
+        public static final int right=0x7f020022;
+        public static final int shield=0x7f020023;
+        public static final int ship1=0x7f020024;
+        public static final int ship1b=0x7f020025;
+        public static final int shot=0x7f020026;
+        public static final int shot2=0x7f020027;
+        public static final int shot3=0x7f020028;
+        public static final int shot3b=0x7f020029;
+        public static final int shot4=0x7f02002a;
+        public static final int shot4b=0x7f02002b;
+        public static final int shot5=0x7f02002c;
+        public static final int shot5b=0x7f02002d;
+        public static final int shot5c=0x7f02002e;
+        public static final int split=0x7f02002f;
+        public static final int startplay=0x7f020030;
+        public static final int vert=0x7f020031;
+        public static final int youwin=0x7f020032;
+    }
+    public static final class id {
+        public static final int begscore=0x7f09001b;
+        public static final int click_to_continue=0x7f09001e;
+        public static final int congrats=0x7f09000e;
+        public static final int continue_game=0x7f090020;
+        public static final int created_by=0x7f090007;
+        public static final int credits_btn=0x7f090012;
+        public static final int easy=0x7f090002;
+        public static final int full_screen=0x7f090000;
+        public static final int hard=0x7f090004;
+        public static final int help_btn=0x7f090014;
+        public static final int high_scores=0x7f090022;
+        public static final int highscore=0x7f090010;
+        public static final int hitbonus=0x7f090018;
+        public static final int hitbonustitle=0x7f090017;
+        public static final int medium=0x7f090003;
+        public static final int menu_buttons=0x7f090001;
+        public static final int new_game=0x7f090021;
+        public static final int paused=0x7f09001f;
+        public static final int peb_full=0x7f090016;
+        public static final int play_btn=0x7f090015;
+        public static final int score=0x7f09000f;
+        public static final int score0=0x7f090009;
+        public static final int score1=0x7f09000a;
+        public static final int score2=0x7f09000b;
+        public static final int score3=0x7f09000c;
+        public static final int score4=0x7f09000d;
+        public static final int scores=0x7f090008;
+        public static final int settings_btn=0x7f090013;
+        public static final int timebonus=0x7f09001a;
+        public static final int timebonustitle=0x7f090019;
+        public static final int title=0x7f090005;
+        public static final int title_bar=0x7f090011;
+        public static final int totalscore=0x7f09001d;
+        public static final int totalscoretitle=0x7f09001c;
+        public static final int version_num=0x7f090006;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int blank=0x7f030000;
+        public static final int choose_difficulty=0x7f030001;
+        public static final int credits=0x7f030002;
+        public static final int high_scores=0x7f030003;
+        public static final int keep_screen_on=0x7f030004;
+        public static final int lose=0x7f030005;
+        public static final int main=0x7f030006;
+        public static final int mainmenu=0x7f030007;
+        public static final int peb=0x7f030008;
+        public static final int play_menu=0x7f030009;
+        public static final int win=0x7f03000a;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int game_over=0x7f040002;
-        public static final int level_cleared=0x7f040003;
-        public static final int menu_pause=0x7f040006;
-        public static final int menu_resume=0x7f040007;
-        public static final int menu_start=0x7f040005;
-        public static final int pause=0x7f040004;
-        public static final int score=0x7f040001;
+        public static final int app_name=0x7f070000;
+        public static final int game_over=0x7f070002;
+        public static final int level_cleared=0x7f070003;
+        public static final int menu_pause=0x7f070006;
+        public static final int menu_resume=0x7f070007;
+        public static final int menu_start=0x7f070005;
+        public static final int pause=0x7f070004;
+        public static final int score=0x7f070001;
+    }
+    public static final class style {
+        public static final int Theme_CustomDialog=0x7f080001;
+        public static final int Theme_Transparent=0x7f080000;
+    }
+    public static final class xml {
+        public static final int settings=0x7f040000;
     }
 }
